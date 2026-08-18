0d79dac57b73aef68d1089325702d962ea39fa04
kristina <kristina.starovoit@gmail.com>
2026-08-18 16:00:28 +0200

    feat: add functionality for assigning random colors for initials in contact list

--- src/app/pages/contacts/components/contact-list/contact-list.ts
+++ src/app/pages/contacts/components/contact-list/contact-list.ts
@@ -12,6 +12,7 @@ import { Contact } from '../../../../shared/interfaces/contact';
 export class ContactList {
   contactService = inject(SupabaseService);
   contacts: Contact[] = [];
+  colors = ["#FF7A00", "#FF5EB3", "#6E52FF", "#9327FF", "#00BEE8", "#1FD7C1", "#FF745E", "#FC71FF", "#FFC701", "#0038FF", "#C3FF2B", "#FFE62B", "#FF4646", "#FFBB2B"]
 
   /** 
    * Loads the contact list from Supabase once the component is initialized. 
@@ -40,10 +41,6 @@ export class ContactList {
    * @returns An array of `[letter, contacts]` tuples, e.g. `['A', [contact1, contact2]]`, 
    * ready to iterate over with `@for` in the template. 
    */
-  // was hier rauskommt: [  
-  //   ['A', [{ name: 'Anton Mayer', ... }]],  
-  //   ['B', [{ name: 'Barbara...', ... }, { name: 'Benedikt...', ... }]]  
-  // ]  
   firstLetter = computed(() => {
     const grouped = Object.groupBy(
       this.sortedContacts(),
@@ -68,4 +65,36 @@ export class ContactList {
     let initials = names[0].charAt(0) + names[1].charAt(0);
     return initials;
   }
+
+  /**
+ * Calculates a numeric sum from the UTF-16 character codes of a name.
+ *
+ * Each character of the name is converted to its corresponding character
+ * code using `charCodeAt()` and added to the total sum.
+ *
+ * @param name - The name whose character codes should be summed.
+ * @returns The sum of all UTF-16 character codes of the name.
+ */
+  getChars(name: string) {
+    let sum = 0;
+    for (let i = 0; i < name.length; i++) {
+      sum += name.charCodeAt(i)
+    }
+    return sum;
+  };
+
+  /**
+   * Determines a color from the available color list based on a name.
+   *
+   * The sum of the name's character codes is used with the modulo operator
+   * to calculate a valid index within the colors array.
+   *
+   * @param name - The name used to determine the color.
+   * @returns The color assigned to the given name.
+   */
+  getColor(name: string) {
+    let sum = this.getChars(name);
+    let colorIndex = sum % this.colors.length;
+    return this.colors[colorIndex];
+  };
 } 
