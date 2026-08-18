97a8a69894110dc0012ffa0ca79b8243eeba73d0
Anja-Isabella Schulz <anja-isabella.schulz@gmx.de>
2026-08-18 09:36:20 +0200

    feat: add nav bar and styles

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Navbar } from './layout/navbar/navbar';
+import { Header } from './layout/header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Navbar, Header],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
