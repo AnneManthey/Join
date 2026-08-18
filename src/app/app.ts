d3d7940865a1da5aee89295a45d838063d820794
kristina <kristina.starovoit@gmail.com>
2026-08-18 08:53:55 +0200

    feat: implement contact list component with initial data and sorting logic

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ContactList } from './pages/contacts/components/contact-list/contact-list';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ContactList],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
--- src/app/pages/contacts/components/contact-list/contact-list.ts
+++ src/app/pages/contacts/components/contact-list/contact-list.ts
@@ -7,5 +7,40 @@ import { Component } from '@angular/core';
   styleUrl: './contact-list.scss',
 })
 export class ContactList {
+  contacts: { name: string; email: string }[] = [
+    {
+      name: 'Benedikt Ziegler',
+      email: 'benedikt@gmail.com'
+    },
+    {
+      name: 'Anton Mayer',
+      email: 'antonm@gmail.com'
+    },
+    {
+      name: 'Barbara Schöneberger',
+      email: 'schoenberger@hotmail.com'
+    },
+        {
+      name: 'Edith Peters',
+      email: 'peters@hotmail.com'
+    }
+
+  ];
+// localeCompare für Umlaute (z.B. damit Özdemir bei = einsortiert wird)
+  ngOnInit() {
+    this.contacts.sort((a, b) => a.name.localeCompare(b.name));
+    console.log(this.contacts);
+  };
+
+  // to do: fallback, falls nur ein name existiert
+  // to do: als pipe auslagern
+  getInitials(name: string) {
+    let names = name.split(" ");
+    console.log(names);
+    let initials = names[0].charAt(0) + names[1].charAt(0);
+    console.log(initials);
+    return initials
+  };
+
 
 }
