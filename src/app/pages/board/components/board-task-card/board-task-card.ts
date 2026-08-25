40321f47fac9361fce7b8ca0ca437ee6b96924a3
Anne Manthey <anne.manthey@pm.me>
2026-08-25 11:37:33 +0200

    feat(board): add board-column and board-task-card components

---
+++ src/app/pages/board/components/board-task-card/board-task-card.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-board-task-card',
+  imports: [],
+  templateUrl: './board-task-card.html',
+  styleUrl: './board-task-card.scss',
+})
+export class BoardTaskCard {}
