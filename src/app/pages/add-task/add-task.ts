3caa2f60f4e0ce15df3e554865e25574fe0c7eaa
kristina <kristina.starovoit@gmail.com>
2026-08-25 18:42:36 +0200

    add: navbar and header in add task component

--- src/app/pages/add-task/add-task.ts
+++ src/app/pages/add-task/add-task.ts
@@ -1,9 +1,12 @@
 import { Component, inject } from '@angular/core';
 import { SupabaseTaskService } from '../../shared/services/supabase-task-service';
+import { Header } from '../../layout/header/header';
+import { Navbar } from '../../layout/navbar/navbar';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-add-task',
-  imports: [],
+  imports: [Header, Navbar, RouterOutlet],
   templateUrl: './add-task.html',
   styleUrl: './add-task.scss',
 })
