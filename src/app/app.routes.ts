40b9816c8425f18a488007ccfb002ee478595b20
Anne Manthey <anne.manthey@pm.me>
2026-08-25 11:08:12 +0200

    feat(board): implement board component with routing and Nav/Header

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
+import { Board } from './pages/board/board';
 import { ContactDetails } from './pages/contacts/components/contact-details/contact-details';
 import { Contacts } from './pages/contacts/contacts';
 
 export const routes: Routes = [
     // Startseite auf Contacts weitergeleitet, damit der Router nicht leer bleibt.
     { path: '', pathMatch: 'full', redirectTo: 'contacts' },
+    { path: 'board', component: Board },
     {
         path: 'contacts',
         component: Contacts,
--- src/app/pages/board/board.ts
+++ src/app/pages/board/board.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { Header } from '../../layout/header/header';
+import { Navbar } from '../../layout/navbar/navbar';
 
 @Component({
   selector: 'app-board',
-  imports: [],
+  imports: [Navbar, Header],
   templateUrl: './board.html',
   styleUrl: './board.scss',
 })
