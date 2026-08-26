64020b07309dc738c7b403b3620978df95e25b72
kristina <kristina.starovoit@gmail.com>
2026-08-26 10:34:23 +0200

    feat: enable reactive form for task submission

--- src/app/pages/add-task/add-task.ts
+++ src/app/pages/add-task/add-task.ts
@@ -3,10 +3,11 @@ import { SupabaseTaskService } from '../../shared/services/supabase-task-service
 import { Header } from '../../layout/header/header';
 import { Navbar } from '../../layout/navbar/navbar';
 import { RouterOutlet } from '@angular/router';
+import { AbstractControl, ValidationErrors, FormControl, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-add-task',
-  imports: [Header, Navbar, RouterOutlet],
+  imports: [Header, Navbar, RouterOutlet, ReactiveFormsModule],
   templateUrl: './add-task.html',
   styleUrl: './add-task.scss',
 })
@@ -18,6 +19,30 @@ export class AddTask {
 
   ngOnInit() {
     this.taskService.getTasks();
+  }
+
+  // TEST ENDE
+
+  taskForm = new FormGroup({
+    title : new FormControl('', {
+      validators: [Validators.required, Validators.minLength(4)]
+    }),
+    description: new FormControl(''),
+    due_date: new FormControl('', {
+      validators: [Validators.required]
+    }),
+    priority: new FormControl('', {
+      validators: [Validators.required]
+    }),
+    assignedTo: new FormControl(''),
+    category: new FormControl('', {
+      validators: [Validators.required]
+    }),
+    subtaskInput: new FormControl('')
+  })
+
+  formSubmit() {
+    console.log('form submitted');
   }
 
 }
