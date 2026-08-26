fa597a05ad7ae82790747d99d44e41424650ee0a
Anja-Isabella Schulz <anja-isabella.schulz@gmx.de>
2026-08-26 17:33:03 +0200

    feat(task-detail-dialog): show due date and assignee avatars with initials

--- src/app/pages/board/components/task-detail-dialog/task-detail-dialog.ts
+++ src/app/pages/board/components/task-detail-dialog/task-detail-dialog.ts
@@ -1,13 +1,17 @@
 import { Component, input, output } from '@angular/core';
 import { Task } from '../../../../shared/interfaces/task';
+import { GetInitialsPipe } from '../../../../shared/pipes/get-initials-pipe';
+import { getColor } from '../../../../shared/utils/contacts-helper';
 
 @Component({
   selector: 'app-task-detail-dialog',
-  imports: [],
+  imports: [GetInitialsPipe],
   templateUrl: './task-detail-dialog.html',
   styleUrl: './task-detail-dialog.scss',
 })
 export class TaskDetailDialog {
+
+   getColor = getColor;
 
   isTaskDetailDialogOpen = input.required<boolean>();
   task = input.required<Task>();
