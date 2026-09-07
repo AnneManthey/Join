2bcc88cafc4ccf2b2c51aa57df192069f383629e
kristina <kristina.starovoit@gmail.com>
2026-09-07 14:11:52 +0200

    feat(register): enhance registration form with password confirmation and policy acceptance

--- src/app/pages/login-home/components/register/register.ts
+++ src/app/pages/login-home/components/register/register.ts
@@ -1,21 +1,28 @@
 import { Component, inject } from '@angular/core';
 import { AuthService } from '../../../../shared/services/auth-service';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { confirmPasswordValidator } from '../../../../shared/utils/confirm-password-validator';
+import { AddContactDialog } from '../../../contacts/components/add-contact-dialog/add-contact-dialog';
 
 @Component({
   selector: 'app-register',
   imports: [ReactiveFormsModule],
   templateUrl: './register.html',
   styleUrl: './register.scss',
+  providers: [AddContactDialog]
 })
 export class Register {
   authService = inject(AuthService);
+  nameValidator = inject(AddContactDialog).namePattern;
+  mailValidator = inject(AddContactDialog).emailDomainPattern;
 
   registerForm = new FormGroup({
-    registerUsername: new FormControl('', { validators: Validators.required }),
-    registerUsermail: new FormControl('', { validators: Validators.required }),
+    registerUsername: new FormControl('', { validators: [Validators.required, Validators.pattern(this.nameValidator)] }),
+    registerUsermail: new FormControl('', { validators: [Validators.required, Validators.pattern(this.mailValidator)]}),
     registerPassword: new FormControl('', { validators: Validators.required }),
-  });
+    confirmPassword: new FormControl('', { validators: Validators.required }),
+    registerPolicy: new FormControl(false, { validators: Validators.requiredTrue})
+  }, { validators: [confirmPasswordValidator()] });
 
   get registerUsername() {
     return this.registerForm.get('registerUsername');
@@ -27,6 +34,10 @@ export class Register {
 
   get registerPassword() {
     return this.registerForm.get('registerPassword');
+  };
+
+  get confirmPassword() {
+    return this.registerForm.get('confirmPassword');
   };
 
   submitRegister() {
